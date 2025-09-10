2af59987564af9ddad4e492251b0c0d3011afa19
pavithranapa17 <pavithra.n@servion.com>
2025-09-10 17:03:10 +0530

    commit from helloworld 3

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const functions = require('@google-cloud/functions-framework');
 // Register an HTTP function with the Functions Framework that will be executed
 // when you make an HTTP request to the deployed function's endpoint.
 functions.http('helloHttp', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World 3!');
 });
